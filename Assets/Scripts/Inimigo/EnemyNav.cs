3fb2d0c65b34629ff1acc9b3b8c989d01e6dd150
Thiago de sena lima <thiagosena316@gmail.com>
2023-11-05 21:42:46 -0300

    Inicío da Implementação do Menu Principal

---
+++ Assets/Scripts/Inimigo/EnemyNav.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private NavMeshAgent inimigo;
+    private Transform point;
+    void Start()
+    {
+        inimigo = GetComponent<NavMeshAgent>();
+        point = GameObject.Find("Point").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inimigo.SetDestination(point.position);
+    }
+}
