8e098e259d99c73cd9d4a3ee4f9f4588b25e548c
ThiagoDeSena <thiagosena316@gmail.com>
2023-11-28 18:38:25 -0300

    finishi select player

--- Assets/Scripts/UI/SelectPlayer/SelectMenu.cs
+++ Assets/Scripts/UI/SelectPlayer/SelectMenu.cs
@@ -12,10 +12,12 @@ public class SelectMenu : MonoBehaviour
     public Animator playselect1, playselect2;
     private Color defaultColor;
     private int characterIndex;
+    private AudioSource audioS;
     // Start is called before the first frame update
     void Start()
     {
         characterIndex = 1;
+        audioS = GetComponent<AudioSource>();
         defaultColor = luciusImage.color;
     }
 
@@ -24,23 +26,41 @@ public class SelectMenu : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow)){
             characterIndex = 1;
+            PlaySound();
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow)){
             characterIndex = 2;
+            PlaySound();
         }
 
         if(characterIndex == 1){
             herculesImage.color = Color.yellow;
             herculesAnim.SetBool("idle",true);
+            playselect1.SetBool("move",true);
             luciusImage.color = defaultColor;
             luciusAnim.SetBool("idle",false);
+            playselect2.SetBool("move",false);
         }
         else if(characterIndex == 2){
             luciusImage.color = Color.yellow;
             luciusAnim.SetBool("idle",true);
+            playselect2.SetBool("move",true);
             herculesImage.color = defaultColor;
             herculesAnim.SetBool("idle",false);
+            playselect1.SetBool("move",false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Return)){
+            FindObjectOfType<GameManager>().characterIndex = characterIndex;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
 
     }
+
+    void PlaySound(){
+        if(!audioS.isPlaying){
+            audioS.Play();
+        }
+    }
+    
 }
