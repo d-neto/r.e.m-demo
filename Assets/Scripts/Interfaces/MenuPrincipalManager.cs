29ff2e7e90792cf3bd51100780b179c34472caf9
Thiago de sena lima <thiagosena316@gmail.com>
2023-11-29 09:52:53 -0300

    Sequência de cenas

--- Assets/Scripts/Interfaces/MenuPrincipalManager.cs
+++ Assets/Scripts/Interfaces/MenuPrincipalManager.cs
@@ -5,14 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuPrincipalManager : MonoBehaviour
 {
-    [SerializeField] private string nomeDoLevelDeJogo;
-    public void infiniteMode(){
-        SceneManager.LoadScene(nomeDoLevelDeJogo);
-    }
+    
 
     void Update(){
         if(Input.anyKey && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2)){
-            infiniteMode();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
