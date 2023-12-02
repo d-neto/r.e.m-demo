4f5a94f2a7be9125e44fb4431360f44018fd3c6d
D. Neto <nneto2670@gmail.com>
2023-12-02 02:02:05 -0300

    feat: update characters selection

--- Assets/Scripts/UI/SelectPlayer/SelectMenu.cs
+++ Assets/Scripts/UI/SelectPlayer/SelectMenu.cs
@@ -3,64 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
 
+[System.Serializable]
+public class PlayerSelectionSettings{
+    public InputMap inputs;
+    public int characterIndex;
+    public GameObject selector;
+    public bool isReady = false;
+    public bool pressedAxis = false;
+}
+
+[System.Serializable]
+public class SelectOption{
+    public bool isSelected = false;
+    public Transform target;
+    public GameObject light;
+    public GameObject player;
+}
 public class SelectMenu : MonoBehaviour
 {
-    public Image herculesImage, luciusImage;
-    public Animator herculesAnim, luciusAnim;
-    
-    public Animator playselect1, playselect2;
-    private Color defaultColor;
-    private int characterIndex;
+    [SerializeField] private List<SelectOption> characters;
+    [SerializeField] private List<PlayerSelectionSettings> players;
+    [SerializeField] private PlayerSelectionSettings player2;
     private AudioSource audioS;
+    [SerializeField] private AudioClip confirmAudio;
+    [SerializeField] private AudioClip errorAudio;
+    [SerializeField] private AudioClip changeAudio;
     // Start is called before the first frame update
     void Start()
     {
-        characterIndex = 1;
+        for(int i = 0; i < players.Count; i++)
+            if(i < characters.Count){
+                players[i].characterIndex = i;
+                players[i].selector.SetActive(true);
+                players[i].selector.transform.position = characters[i].target.position;
+                characters[i].light.SetActive(true);
+            }
         audioS = GetComponent<AudioSource>();
-        defaultColor = luciusImage.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow)){
-            characterIndex = 1;
-            PlaySound();
-        }
-        else if(Input.GetKeyDown(KeyCode.RightArrow)){
-            characterIndex = 2;
-            PlaySound();
+
+        for(int i = 0; i < players.Count; i++){
+            if(players[i].isReady){
+                continue;
+            }
+
+            if(!characters[players[i].characterIndex].isSelected)
+                characters[players[i].characterIndex].light.SetActive(false);
+
+            if(Input.GetAxisRaw(players[i].inputs.horizontalAxis) > 0 && !players[i].pressedAxis){
+                players[i].characterIndex++;
+                players[i].pressedAxis = true;
+                PlaySound(changeAudio);
+            }else if(Input.GetAxisRaw(players[i].inputs.horizontalAxis) < 0 && !players[i].pressedAxis){
+                players[i].characterIndex--;
+                players[i].pressedAxis = true;
+                PlaySound(changeAudio);
+            }else if(Input.GetAxisRaw(players[i].inputs.horizontalAxis) == 0) players[i].pressedAxis = false;
+
+            if(players[i].characterIndex >= characters.Count) players[i].characterIndex = characters.Count-1;
+            if(players[i].characterIndex < 0) players[i].characterIndex = 0;
+            players[i].selector.transform.position = characters[players[i].characterIndex].target.position;
+            characters[players[i].characterIndex].light.SetActive(true);
+            
+            if(Input.GetButtonDown(players[i].inputs.confirm)){
+                if(characters[players[i].characterIndex].isSelected){
+                    PlaySound(errorAudio, 0.8f);
+                    continue;
+                }
+                PlaySound(confirmAudio);
+                characters[players[i].characterIndex].isSelected = true;
+                characters[players[i].characterIndex].light.SetActive(true);
+                characters[players[i].characterIndex].light.GetComponent<Light2D>().intensity = 0.3f;
+                players[i].selector.SetActive(false);
+                players[i].isReady = true;
+                VerifyAllPlayers();
+            }
         }
 
-        if(characterIndex == 1){
-            herculesImage.color = Color.yellow;
-            herculesAnim.SetBool("idle",true);
-            playselect1.SetBool("move",true);
-            luciusImage.color = defaultColor;
-            luciusAnim.SetBool("idle",false);
-            playselect2.SetBool("move",false);
-        }
-        else if(characterIndex == 2){
-            luciusImage.color = Color.yellow;
-            luciusAnim.SetBool("idle",true);
-            playselect2.SetBool("move",true);
-            herculesImage.color = defaultColor;
-            herculesAnim.SetBool("idle",false);
-            playselect1.SetBool("move",false);
-        }
-
-        if(Input.GetKeyDown(KeyCode.Return)){
-            FindObjectOfType<GameManager>().characterIndex = characterIndex;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if(Input.GetButtonDown(player2.inputs.confirm) && !players.Contains(player2)){
+            players.Add(player2);
+            player2.selector.SetActive(true);
+            player2.characterIndex = characters.Count-1;
         }
 
     }
 
-    void PlaySound(){
+    void PlaySound(AudioClip audio, float volume = 0.4f){
         if(!audioS.isPlaying){
-            audioS.Play();
+            audioS.PlayOneShot(audio, volume);
         }
+    }
+
+    void VerifyAllPlayers(){
+        bool allReady = true;
+        for(int i = 0; i < players.Count; i++){
+            if(!players[i].isReady){
+                allReady = false;
+                break;
+            }
+        }
+        if(!allReady) return;
+
+
     }
     
 }
