63ac9c651367b795a040dc51340a542a0e0887af
D. Neto <nneto2670@gmail.com>
2023-12-02 14:14:50 -0300

    feat: update start game UI

---
+++ Assets/Scripts/Util/Texts/CharacterWobble.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterWobble : MonoBehaviour
+{
+    TMP_Text textMesh;
+
+    Mesh mesh;
+
+    [SerializeField] Vector2 values;
+    [SerializeField] Vector2 offset;
+
+    Vector3[] vertices;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.ForceMeshUpdate();
+        mesh = textMesh.mesh;
+        vertices = mesh.vertices;
+
+        for (int i = 0; i < textMesh.textInfo.characterCount; i++)
+        {
+            TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
+
+            int index = c.vertexIndex;
+
+            Vector3 offset = Wobble(Time.time + i);
+            vertices[index] += offset;
+            vertices[index + 1] += offset;
+            vertices[index + 2] += offset;
+            vertices[index + 3] += offset;
+        }
+
+        mesh.vertices = vertices;
+        textMesh.canvasRenderer.SetMesh(mesh);
+    }
+
+    Vector2 Wobble(float time) {
+        return new Vector2(Mathf.Sin(time*values.x)*offset.x, Mathf.Cos(time*values.y)*offset.y);
+    }
+}
